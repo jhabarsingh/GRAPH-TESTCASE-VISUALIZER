4530978c7763db59b6728d9a97855e0ef552c537
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-17 00:24:46 +0530

    Added react router

--- graphtestcasevisualize/src/App.js
+++ graphtestcasevisualize/src/App.js
@@ -1,11 +1,26 @@
-import Graph from './components/Graphs.js'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Graphs from './components/Graphs.js'
+import Home from './components/Home.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Graph />
-      </header>
+        <Router>
+          <Switch>
+            <Route path="/visualize">
+              <Graphs />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
