00ec1c76e2878b3be8e6c734c8668a0ebb3b2d7d
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-17 00:49:51 +0530

    Added HomeCard component

--- graphtestcasevisualize/src/components/Home.js
+++ graphtestcasevisualize/src/components/Home.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import HomeCard from './HomeCard.js'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
-  },
-}));
-
-export default function SimplePaper() {
-  const classes = useStyles();
-
+export default function SimpleContainer() {
   return (
-    <div className={classes.root}>
-      <Paper elevation={0} />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container style={{backgroundColor: "white"}}>
+        <HomeCard />          
+      </Container>
+    </React.Fragment>
   );
 }
