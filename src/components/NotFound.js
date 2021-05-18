a25dabbaee40258043edb99b792bc7c17dddc8a4
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-18 18:37:57 +0530

    Added 404not found page

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return(
+        <img
+            src="../../public/404.png"
+            style={{
+                width: "100vw",
+                height: "100vh"
+            }}
+        >
+        </img>
+    )
+}
